cc351e86698d14e0304faa46ad33ebf089d03668
Afforia <aforia@protonmail.com>
2019-02-22 12:41:51 +0300

    better sequence

--- new/main.c
+++ new/main.c
@@ -13,4 +13,46 @@
 #include "fillit.h"
 #include "libft/libft.h"
 
- 
+int		main(int argc, char **argv)
+{
+	int			fd;
+	int			ret;
+	char		buf[256]; //malloc NEED
+	int			count;
+
+	count = 0;
+	if (argc != 2)
+	{
+		ft_putstr("usage: fillit\n");
+		return (1);
+	}
+	
+	fd = open(argv[1]);
+	while ((ret = read(fd, buf, 21)) > 0)
+	{
+		buf[ret] = '\0';
+		if (ret == 21)
+		{
+			if (check_not_last_buf(buf)) //how should i exit from here
+				return (0);
+		}
+		if (ret < 21)
+		{
+			if (check_last(buf)) //how should i exit from here
+				return (0);
+		}
+		count++;
+	}
+	if (count > 26 || count <= 0)
+	{
+		close (fd);
+		return (1);
+	}
+	if ((solution(count, buf))) //mb need if solution wasnt found
+	{
+		close (fd);
+		return (1);
+	}
+	close(fd);
+	return (0);
+}
